961f5ff843bd2692a846678766b1b3bde72cc9dd
Matt Westcott <matt@west.co.tt>
2023-04-27 18:02:56 +0100

    Cache results

--- queryish/__init__.py
+++ queryish/__init__.py
@@ -1,6 +1,16 @@
 class Queryish:
+    def __init__(self):
+        self._results = None
+
     def run_query(self):
         raise NotImplementedError
 
     def __iter__(self):
-        yield from self.run_query()
+        if self._results is None:
+            results = []
+            for result in self.run_query():
+                results.append(result)
+                yield result
+            self._results = results
+        else:
+            yield from self._results
--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,12 @@ from queryish import Queryish
 
 
 class CounterSource(Queryish):
+    def __init__(self):
+        super().__init__()
+        self.run_query_call_count = 0
+
     def run_query(self):
+        self.run_query_call_count += 1
         for i in range(0, 10):
             yield i
 
@@ -13,3 +18,10 @@ class TestQueryish(TestCase):
     def test_get_results_as_list(self):
         qs = CounterSource()
         self.assertEqual(list(qs), list(range(0, 10)))
+        self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_query_is_only_run_once(self):
+        qs = CounterSource()
+        list(qs)
+        list(qs)
+        self.assertEqual(qs.run_query_call_count, 1)
