b3a46898fc9d6f7451856b6ae158ef4c10a19685
Matt Westcott <matt@west.co.tt>
2023-04-27 20:03:49 +0100

    Implement counts and slicing

--- queryish/__init__.py
+++ queryish/__init__.py
@@ -1,9 +1,20 @@
+import copy
+
 class Queryish:
     def __init__(self):
         self._results = None
+        self._count = None
+        self.offset = 0
+        self.limit = None
 
     def run_query(self):
         raise NotImplementedError
+
+    def run_count(self):
+        count = 0
+        for i in self:
+            count += 1
+        return count
 
     def __iter__(self):
         if self._results is None:
@@ -14,3 +25,39 @@ class Queryish:
             self._results = results
         else:
             yield from self._results
+
+    def count(self):
+        if self._count is None:
+            self._count = self.run_count()
+        return self._count
+
+    def __len__(self):
+        return self.count()
+
+    def clone(self, **kwargs):
+        clone = copy.copy(self)
+        clone._results = None
+        clone._count = None
+        for key, value in kwargs.items():
+            setattr(clone, key, value)
+        return clone
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            if key.step is not None:
+                raise ValueError("%r does not support slicing with a step" % self.__class__.__name__)
+
+            new_offset = self.offset + (key.start or 0)
+            if key.stop is None:
+                # no new limit imposed, but need to adjust any existing limit to account for the new offset
+                if self.limit is None:
+                    new_limit = None
+                else:
+                    new_limit = self.limit - (key.start or 0)
+            else:
+                # new limit imposed
+                new_limit = key.stop - (key.start or 0)
+
+            return self.clone(offset=new_offset, limit=new_limit)
+        else:
+            raise NotImplementedError("%r does not support indexing" % self.__class__.__name__)
--- tests/test.py
+++ tests/test.py
@@ -3,15 +3,36 @@ from unittest import TestCase
 from queryish import Queryish
 
 
-class CounterSource(Queryish):
+class CounterSourceWithoutCount(Queryish):
     def __init__(self):
         super().__init__()
         self.run_query_call_count = 0
 
+    def _get_real_limits(self):
+        start = min(self.offset, 10)
+        if self.limit is not None:
+            stop = min(self.offset + self.limit, 10)
+        else:
+            stop = 10
+
+        return (start, stop)
+
     def run_query(self):
         self.run_query_call_count += 1
-        for i in range(0, 10):
+        start, stop = self._get_real_limits()
+        for i in range(start, stop):
             yield i
+
+
+class CounterSource(CounterSourceWithoutCount):
+    def __init__(self):
+        super().__init__()
+        self.run_count_call_count = 0
+
+    def run_count(self):
+        self.run_count_call_count += 1
+        start, stop = self._get_real_limits()
+        return stop - start
 
 
 class TestQueryish(TestCase):
@@ -25,3 +46,85 @@ class TestQueryish(TestCase):
         list(qs)
         list(qs)
         self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_count_uses_results_by_default(self):
+        qs = CounterSourceWithoutCount()
+        self.assertEqual(qs.count(), 10)
+        self.assertEqual(qs.count(), 10)
+        self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_count_does_not_use_results_when_run_count_provided(self):
+        qs = CounterSource()
+        self.assertEqual(qs.count(), 10)
+        self.assertEqual(qs.count(), 10)
+        self.assertEqual(qs.run_count_call_count, 1)
+        self.assertEqual(qs.run_query_call_count, 0)
+
+    def test_len_uses_count(self):
+        qs = CounterSource()
+        self.assertEqual(len(qs), 10)
+        self.assertEqual(qs.run_count_call_count, 1)
+        self.assertEqual(qs.run_query_call_count, 0)
+
+    def test_slicing(self):
+        qs = CounterSource()[1:3]
+        self.assertEqual(qs.offset, 1)
+        self.assertEqual(qs.limit, 2)
+        self.assertEqual(list(qs), [1, 2])
+        self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_slicing_without_start(self):
+        qs = CounterSource()[:3]
+        self.assertEqual(qs.offset, 0)
+        self.assertEqual(qs.limit, 3)
+        self.assertEqual(list(qs), [0, 1, 2])
+        self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_slicing_without_stop(self):
+        qs = CounterSource()[3:]
+        self.assertEqual(qs.offset, 3)
+        self.assertEqual(qs.limit, None)
+        self.assertEqual(list(qs), [3, 4, 5, 6, 7, 8, 9])
+        self.assertEqual(qs.run_query_call_count, 1)
+
+    def test_multiple_slicing(self):
+        qs1 = CounterSource()
+        qs2 = qs1[1:9]
+        self.assertEqual(qs2.offset, 1)
+        self.assertEqual(qs2.limit, 8)
+        qs3 = qs2[2:4]
+        self.assertEqual(qs3.offset, 3)
+        self.assertEqual(qs3.limit, 2)
+
+        self.assertEqual(list(qs3), [3, 4])
+        self.assertEqual(qs1.run_query_call_count, 0)
+        self.assertEqual(qs2.run_query_call_count, 0)
+        self.assertEqual(qs3.run_query_call_count, 1)
+
+    def test_multiple_slicing_without_start(self):
+        qs1 = CounterSource()
+        qs2 = qs1[1:9]
+        self.assertEqual(qs2.offset, 1)
+        self.assertEqual(qs2.limit, 8)
+        qs3 = qs2[:4]
+        self.assertEqual(qs3.offset, 1)
+        self.assertEqual(qs3.limit, 4)
+
+        self.assertEqual(list(qs3), [1, 2, 3, 4])
+        self.assertEqual(qs1.run_query_call_count, 0)
+        self.assertEqual(qs2.run_query_call_count, 0)
+        self.assertEqual(qs3.run_query_call_count, 1)
+
+    def test_multiple_slicing_without_stop(self):
+        qs1 = CounterSource()
+        qs2 = qs1[1:9]
+        self.assertEqual(qs2.offset, 1)
+        self.assertEqual(qs2.limit, 8)
+        qs3 = qs2[2:]
+        self.assertEqual(qs3.offset, 3)
+        self.assertEqual(qs3.limit, 6)
+
+        self.assertEqual(list(qs3), [3, 4, 5, 6, 7, 8])
+        self.assertEqual(qs1.run_query_call_count, 0)
+        self.assertEqual(qs2.run_query_call_count, 0)
+        self.assertEqual(qs3.run_query_call_count, 1)
