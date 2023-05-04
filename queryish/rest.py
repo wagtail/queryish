1dff6ab72f23a048f12dc9463d8fc60325c81263
Matt Westcott <matt@west.co.tt>
2023-05-04 13:27:53 +0100

    Cache API responses

--- queryish/rest.py
+++ queryish/rest.py
@@ -4,13 +4,27 @@ from queryish import Queryish
 
 
 class APISource(Queryish):
+    def __init__(self):
+        super().__init__()
+        self._responses = {}  # cache for API responses
+
     def run_query(self):
-        response_json = requests.get(
-            self.base_url,
-            headers={"Accept": "application/json"},
-        ).json()
+        response_json = self.fetch_api_response()
         if self.limit is None:
             stop = None
         else:
             stop = self.offset + self.limit
         return response_json[self.offset:stop]
+
+    def fetch_api_response(self, params=None):
+        # construct a hashable key for the params
+        if params is None:
+            params = {}
+        key = tuple(sorted(params.items()))
+        if key not in self._responses:
+            self._responses[key] = requests.get(
+                self.base_url,
+                params=params,
+                headers={"Accept": "application/json"},
+            ).json()
+        return self._responses[key]
