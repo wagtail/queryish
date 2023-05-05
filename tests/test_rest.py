a2b134e06ab0351b0b2b755de04839006e366712
Matt Westcott <matt@west.co.tt>
2023-05-05 17:35:21 +0100

    Update API tests to include ID

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -5,25 +5,23 @@ from responses import matchers
 from queryish.rest import APISource
 
 
-class UnpaginatedCountryAPISource(APISource):
+class CountryAPISource(APISource):
     base_url = "http://example.com/api/countries/"
-    filter_fields = ["name", "continent"]
-    ordering_fields = ["name", "continent"]
+    filter_fields = ["id", "name", "continent"]
+    ordering_fields = ["id", "name", "continent"]
 
 
-class LimitOffsetPaginatedCountryAPISource(APISource):
-    base_url = "http://example.com/api/countries/"
+class UnpaginatedCountryAPISource(CountryAPISource):
+    pass
+
+
+class LimitOffsetPaginatedCountryAPISource(CountryAPISource):
     pagination_style = "offset-limit"
-    filter_fields = ["name", "continent"]
-    ordering_fields = ["name", "continent"]
 
 
-class PageNumberPaginatedCountryAPISource(APISource):
-    base_url = "http://example.com/api/countries/"
+class PageNumberPaginatedCountryAPISource(CountryAPISource):
     pagination_style = "page-number"
     page_size = 2
-    filter_fields = ["name", "continent"]
-    ordering_fields = ["name", "continent"]
 
 
 class TestAPISource(TestCase):
@@ -34,22 +32,27 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 1,
                         "name": "France",
                         "continent": "europe"
                     },
                     {
+                        "id": 2,
                         "name": "Germany",
                         "continent": "europe"
                     },
                     {
+                        "id": 3,
                         "name": "Italy",
                         "continent": "europe"
                     },
                     {
+                        "id": 4,
                         "name": "Japan",
                         "continent": "asia"
                     },
                     {
+                        "id": 5,
                         "name": "China",
                         "continent": "asia"
                     }
@@ -62,8 +65,8 @@ class TestAPISource(TestCase):
         results = UnpaginatedCountryAPISource()[1:3]
         self.assertFalse(results.ordered)
         self.assertEqual(list(results), [
-            {"name": "Germany", "continent": "europe"},
-            {"name": "Italy", "continent": "europe"},
+            {"id": 2, "name": "Germany", "continent": "europe"},
+            {"id": 3, "name": "Italy", "continent": "europe"},
         ])
 
     @responses.activate
@@ -78,6 +81,7 @@ class TestAPISource(TestCase):
                     "previous": null,
                     "results": [
                         {
+                            "id": 1,
                             "name": "France",
                             "continent": "europe"
                         }
@@ -96,10 +100,12 @@ class TestAPISource(TestCase):
                     "previous": null,
                     "results": [
                         {
+                            "id": 1,
                             "name": "France",
                             "continent": "europe"
                         },
                         {
+                            "id": 2,
                             "name": "Germany",
                             "continent": "europe"
                         }
@@ -118,10 +124,12 @@ class TestAPISource(TestCase):
                     "previous": "http://example.com/api/countries/?limit=2",
                     "results": [
                         {
+                            "id": 3,
                             "name": "Italy",
                             "continent": "europe"
                         },
                         {
+                            "id": 4,
                             "name": "Japan",
                             "continent": "asia"
                         }
@@ -140,6 +148,7 @@ class TestAPISource(TestCase):
                     "previous": "http://example.com/api/countries/?limit=2&offset=2",
                     "results": [
                         {
+                            "id": 5,
                             "name": "China",
                             "continent": "asia"
                         }
@@ -158,10 +167,12 @@ class TestAPISource(TestCase):
                     "previous": "http://example.com/api/countries/?limit=2",
                     "results": [
                         {
+                            "id": 3,
                             "name": "Italy",
                             "continent": "europe"
                         },
                         {
+                            "id": 4,
                             "name": "Japan",
                             "continent": "asia"
                         }
@@ -173,12 +184,12 @@ class TestAPISource(TestCase):
         self.assertEqual(LimitOffsetPaginatedCountryAPISource().count(), 5)
 
         full_results = list(LimitOffsetPaginatedCountryAPISource())
-        self.assertEqual(full_results[2], {"name": "Italy", "continent": "europe"})
+        self.assertEqual(full_results[2], {"id": 3, "name": "Italy", "continent": "europe"})
 
         partial_results = list(LimitOffsetPaginatedCountryAPISource()[2:4])
         self.assertEqual(partial_results, [
-            {"name": "Italy", "continent": "europe"},
-            {"name": "Japan", "continent": "asia"},
+            {"id": 3, "name": "Italy", "continent": "europe"},
+            {"id": 4, "name": "Japan", "continent": "asia"},
         ])
 
     @responses.activate
@@ -193,10 +204,12 @@ class TestAPISource(TestCase):
                     "previous": null,
                     "results": [
                         {
+                            "id": 1,
                             "name": "France",
                             "continent": "europe"
                         },
                         {
+                            "id": 2,
                             "name": "Germany",
                             "continent": "europe"
                         }
@@ -214,10 +227,12 @@ class TestAPISource(TestCase):
                     "previous": "http://example.com/api/countries/",
                     "results": [
                         {
+                            "id": 3,
                             "name": "Italy",
                             "continent": "europe"
                         },
                         {
+                            "id": 4,
                             "name": "Japan",
                             "continent": "asia"
                         }
@@ -235,6 +250,7 @@ class TestAPISource(TestCase):
                     "previous": "http://example.com/api/countries/?page=2",
                     "results": [
                         {
+                            "id": 5,
                             "name": "China",
                             "continent": "asia"
                         }
@@ -246,12 +262,12 @@ class TestAPISource(TestCase):
         self.assertEqual(PageNumberPaginatedCountryAPISource().count(), 5)
 
         full_results = list(PageNumberPaginatedCountryAPISource())
-        self.assertEqual(full_results[2], {"name": "Italy", "continent": "europe"})
+        self.assertEqual(full_results[2], {"id": 3, "name": "Italy", "continent": "europe"})
 
         partial_results = list(PageNumberPaginatedCountryAPISource()[2:4])
         self.assertEqual(partial_results, [
-            {"name": "Italy", "continent": "europe"},
-            {"name": "Japan", "continent": "asia"},
+            {"id": 3, "name": "Italy", "continent": "europe"},
+            {"id": 4, "name": "Japan", "continent": "asia"},
         ])
 
     @responses.activate
@@ -262,10 +278,12 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 4,
                         "name": "Japan",
                         "continent": "asia"
                     },
                     {
+                        "id": 5,
                         "name": "China",
                         "continent": "asia"
                     }
@@ -278,22 +296,27 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 1,
                         "name": "France",
                         "continent": "europe"
                     },
                     {
+                        "id": 2,
                         "name": "Germany",
                         "continent": "europe"
                     },
                     {
+                        "id": 3,
                         "name": "Italy",
                         "continent": "europe"
                     },
                     {
+                        "id": 4,
                         "name": "Japan",
                         "continent": "asia"
                     },
                     {
+                        "id": 5,
                         "name": "China",
                         "continent": "asia"
                     }
@@ -307,8 +330,8 @@ class TestAPISource(TestCase):
         # filter should not affect the original queryset
         self.assertEqual(all_results.count(), 5)
         self.assertEqual(list(results), [
-            {"name": "Japan", "continent": "asia"},
-            {"name": "China", "continent": "asia"},
+            {"id": 4, "name": "Japan", "continent": "asia"},
+            {"id": 5, "name": "China", "continent": "asia"},
         ])
 
     @responses.activate
@@ -320,6 +343,7 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 4,
                         "name": "Japan",
                         "continent": "asia"
                     }
@@ -329,12 +353,12 @@ class TestAPISource(TestCase):
 
         results = UnpaginatedCountryAPISource().filter(continent="asia", name="Japan")
         self.assertEqual(results.count(), 1)
-        self.assertEqual(list(results), [{"name": "Japan", "continent": "asia"}])
+        self.assertEqual(list(results), [{"id": 4, "name": "Japan", "continent": "asia"}])
 
         # filters can also be chained
         results = UnpaginatedCountryAPISource().filter(continent="asia").filter(name="Japan")
         self.assertEqual(results.count(), 1)
-        self.assertEqual(list(results), [{"name": "Japan", "continent": "asia"}])
+        self.assertEqual(list(results), [{"id": 4, "name": "Japan", "continent": "asia"}])
 
     @responses.activate
     def test_ordering(self):
@@ -344,10 +368,12 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 5,
                         "name": "China",
                         "continent": "asia"
                     },
                     {
+                        "id": 4,
                         "name": "Japan",
                         "continent": "asia"
                     }
@@ -359,8 +385,8 @@ class TestAPISource(TestCase):
         self.assertTrue(results.ordered)
         self.assertEqual(results.count(), 2)
         self.assertEqual(list(results), [
-            {"name": "China", "continent": "asia"},
-            {"name": "Japan", "continent": "asia"},
+            {"id": 5, "name": "China", "continent": "asia"},
+            {"id": 4, "name": "Japan", "continent": "asia"},
         ])
 
     @responses.activate
@@ -371,6 +397,7 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 1,
                         "name": "France",
                         "continent": "europe"
                     }
@@ -392,14 +419,17 @@ class TestAPISource(TestCase):
             body="""
                 [
                     {
+                        "id": 1,
                         "name": "France",
                         "continent": "europe"
                     },
                     {
+                        "id": 2,
                         "name": "Germany",
                         "continent": "europe"
                     },
                     {
+                        "id": 3,
                         "name": "Italy",
                         "continent": "europe"
                     }
@@ -409,7 +439,7 @@ class TestAPISource(TestCase):
 
         self.assertEqual(
             UnpaginatedCountryAPISource().get(name="France"),
-            {"name": "France", "continent": "europe"}
+            {"id": 1, "name": "France", "continent": "europe"}
         )
         with self.assertRaises(ValueError):
             UnpaginatedCountryAPISource().get(name="Wakanda")
